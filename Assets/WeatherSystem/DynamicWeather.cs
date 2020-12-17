32963a1279d6d244bc7899dc90a75d69975284f5
EmmanuelN-L <46698046+EmmanuelN-L@users.noreply.github.com>
2020-12-16 22:41:59 -0500

    Initial commit

---
+++ Assets/WeatherSystem/DynamicWeather.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DynamicWeather : MonoBehaviour
+{
+
+    public enum WeatherStates
+    {
+        WeatherChoice,
+        Sunny,
+        Overcast,
+        Thunderstorm,
+        Rainy
+    }
+
+    void WeatherChoice()
+    {
+
+    }
+}
